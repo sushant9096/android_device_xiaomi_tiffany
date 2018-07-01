e2257e74491356bd3840496c880977646c132254
Mika-Pikazo <1376106928@qq.com>
2018-07-01 11:29:14 +0800

    Aex bring up.

--- aosp_tiffany.mk
+++ aosp_tiffany.mk
@@ -18,8 +18,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Aosp stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Inherit from tiffany device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -27,7 +27,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := tiffany
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_tiffany
+PRODUCT_NAME := aosp_tiffany
 PRODUCT_MODEL := MI 5X
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
