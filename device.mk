78cea9495a1d7c057d7e392e88e75061c9f5ad84
litakasumi <1376106928@qq.com>
2018-08-03 18:47:33 +0800

    tiffany: Remove unnecessary languages_full.mk include * It's already included in full_base_telephony.

--- device.mk
+++ device.mk
@@ -1,5 +1,6 @@
 #
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,8 +15,9 @@
 # limitations under the License.
 #
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
+# Get non-open-source specific aspects
 $(call inherit-product, vendor/xiaomi/tiffany/tiffany-vendor.mk)
 
 # Overlays
